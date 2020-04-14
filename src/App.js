b0dbbb53e1fb87f69f4752d6837d2625a7bc9818
kimvision <ilhank@naver.com>
2020-04-14 20:07:42 +0900

    Hello github

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello
+        </p>
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
