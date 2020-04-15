a58bcb8d7379a854294d5fd169b9bac2011e9c1a
kimvision <ilhank@naver.com>
2020-04-15 12:46:40 +0900

    React-management

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import React from 'react';
 import './App.css';
 import Customer from './component/Customer';
 
-
-const customer={
-  name:'홍길동',
-  birthday:'1234555',
-  gender:'남자',
-  job:'대학생'
-}
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '나동빈',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '960305',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '921205',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+  ]
+  
 
 function App() 
 {
   return (
     <div>
-      <Customer name={customer.name} birthday={customer.birthday} gender={customer.gender} job={customer.job}/>
+      {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/> ); }) }
     </div>
   );
 }
@@ -24,22 +44,20 @@ function App()
 export default App;
 
 
+  
 
+// const customer={
+//   name:'홍길동',
+//   birthday:'1234555',
+//   gender:'남자',
+//   job:'대학생'
+// }
 
-
-/*
-function App() 
-{
-  return (
-    <div className="gray-background">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello ..
-          asdfsaD
-        </p>
-   
-      </header>
-    </div>
-  );
-}*/
+// function App() 
+// {
+//   return (
+//     <div>
+//       <Customer name={customer.name} birthday={customer.birthday} gender={customer.gender} job={customer.job}/>
+//     </div>
+//   );
+// }
--- src/component/Customer.js
+++ src/component/Customer.js
@@ -1,17 +1,36 @@
 import  React from 'react';
 
 
-class Customer extends React.Component
-{
-    render()
-    {
+class Customer extends React.Component {
+    render() {
         return (
-        <div>
-            <h2>{this.props.name}</h2>
-            <p>{this.props.birthday}</p>
-            <p>{this.props.gender}</p>
-            <p>{this.props.job}</p>
-        </div>
+            <div>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
         )
     }
 }
@@ -30,6 +49,22 @@ export default Customer;
 //             <p>12354</p>
 //             <p>남자</p>
 //             <p>대학생</p>
+//         </div>
+//         )
+//     }
+// }
+
+
+// class Customer extends React.Component
+// {
+//     render()
+//     {
+//         return (
+//         <div>
+//             <h2>{this.props.name}</h2>
+//             <p>{this.props.birthday}</p>
+//             <p>{this.props.gender}</p>
+//             <p>{this.props.job}</p>
 //         </div>
 //         )
 //     }
